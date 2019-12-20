1c5acd5609855f84a2f12843caa5d0bff5e16f46
Sarath Kaul <sarath.kaul@searce.com>
2019-12-20 12:02:21 +0530

    Added templates

--- myapp/views.py
+++ myapp/views.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
 # Create your views here.
+
+def hello(request):
+    today = datetime.datetime.now().date()
+    return render(request, "hello.html", {"today": today})
