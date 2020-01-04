81d6d4bf887f6adad02dc76506e6c82ba26bdbb7
Sarath Kaul <sarath.kaul@searce.com>
2020-01-04 11:58:44 +0530

    Model View Changes

--- player/views.py
+++ player/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from myapp.models import Game
 
 
 def home(request):
-    return render(request, "player/home.html")
+    first_player = Game.objects.filter(
+        firstPlayer = request.user,
+        status = "F"
+    )
+    second_player = Game.objects.filter(
+        secondPlayer = request.user,
+        status = "S"
+    )
+    all_games = list(first_player) + list(second_player)
+
+
+    return render(request, "player/home.html", {"games": all_games})
