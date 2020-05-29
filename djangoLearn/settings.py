04ab37d6eb86716b008cac74263e95329dd35243
Sarath Kaul <kaul.sarath@gmail.com>
2020-05-29 05:35:56 +0000

    Settings Changes

--- djangoLearn/settings.py
+++ djangoLearn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY","")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["skaul05-tictactoe.herokuapp.com", "*"]
+ALLOWED_HOSTS = ["skaul05-tictactoe.herokuapp.com"]
 
 
 # Application definition
